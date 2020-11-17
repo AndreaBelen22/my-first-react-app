8d47d2b07ef98e50666d1b4c5d066708d10f868a
AndreaBelen22 <belenorregoa@gmail.com>
2020-11-17 18:36:53 +0100

    redirects added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello <strong>World!</strong>
         </a>
       </header>
     </div>
