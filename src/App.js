ebef1863a1b58822d8215113081c8968b317af80
AndreaBelen22 <belenorregoa@gmail.com>
2020-11-17 21:59:27 +0100

    pokemon added2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Title from './components/Title'
 import Pokemon from "./components/Pokemon"
-
+import React from "react";
 
 
 function App() {
-  return (
-    <div className="App">
-      <main>
-        <Title content="Some example title" />
-        <Pokemon />
-      </main>
-      </div>
-  );};
+    return (
+        <div className="App">
+            <main>
+                <Title content="Some example title"/>
+                <Pokemon/>
+            </main>
+        </div>
+    );
+}
+
+export default App;
